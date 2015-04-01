860cb185773a39095152428ac80b3fb86457d785
Yuan Yifan <yuanyifan1993@foxmail.com>
2015-04-01 13:59:49 +0800

    Upload Files

---
+++ EasyNote/Form1.Designer.vb
@@ -0,0 +1,163 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.CmbSelect = New System.Windows.Forms.ComboBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.OperateToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearThisToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearAllToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EnglishToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ChineseToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.KeepFrontMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HideToolToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FontDlg = New System.Windows.Forms.FontDialog()
+        Me.EvText = New System.Windows.Forms.TextBox()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'CmbSelect
+        '
+        Me.CmbSelect.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.CmbSelect.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.CmbSelect.Font = New System.Drawing.Font("微软雅黑", 10.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.CmbSelect.FormattingEnabled = True
+        Me.CmbSelect.ItemHeight = 20
+        Me.CmbSelect.Location = New System.Drawing.Point(5, 27)
+        Me.CmbSelect.Name = "CmbSelect"
+        Me.CmbSelect.Size = New System.Drawing.Size(385, 28)
+        Me.CmbSelect.TabIndex = 1
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OperateToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(679, 26)
+        Me.MenuStrip1.TabIndex = 3
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'OperateToolStripMenuItem
+        '
+        Me.OperateToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ClearThisToolStripMenuItem, Me.ClearAllToolStripMenuItem, Me.KeepFrontMenuItem, Me.HideToolToolStripMenuItem, Me.ToolStripMenuItem1, Me.ExitToolStripMenuItem})
+        Me.OperateToolStripMenuItem.Font = New System.Drawing.Font("Microsoft YaHei UI", 10.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.OperateToolStripMenuItem.Name = "OperateToolStripMenuItem"
+        Me.OperateToolStripMenuItem.Size = New System.Drawing.Size(88, 22)
+        Me.OperateToolStripMenuItem.Text = "Operation"
+        '
+        'ClearThisToolStripMenuItem
+        '
+        Me.ClearThisToolStripMenuItem.Name = "ClearThisToolStripMenuItem"
+        Me.ClearThisToolStripMenuItem.Size = New System.Drawing.Size(181, 22)
+        Me.ClearThisToolStripMenuItem.Text = "Clear This"
+        '
+        'ClearAllToolStripMenuItem
+        '
+        Me.ClearAllToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.EnglishToolStripMenuItem, Me.ChineseToolStripMenuItem})
+        Me.ClearAllToolStripMenuItem.Name = "ClearAllToolStripMenuItem"
+        Me.ClearAllToolStripMenuItem.Size = New System.Drawing.Size(181, 22)
+        Me.ClearAllToolStripMenuItem.Text = "Language"
+        '
+        'EnglishToolStripMenuItem
+        '
+        Me.EnglishToolStripMenuItem.Name = "EnglishToolStripMenuItem"
+        Me.EnglishToolStripMenuItem.Size = New System.Drawing.Size(132, 22)
+        Me.EnglishToolStripMenuItem.Text = "English"
+        '
+        'ChineseToolStripMenuItem
+        '
+        Me.ChineseToolStripMenuItem.Name = "ChineseToolStripMenuItem"
+        Me.ChineseToolStripMenuItem.Size = New System.Drawing.Size(132, 22)
+        Me.ChineseToolStripMenuItem.Text = "简体中文"
+        '
+        'KeepFrontMenuItem
+        '
+        Me.KeepFrontMenuItem.Name = "KeepFrontMenuItem"
+        Me.KeepFrontMenuItem.Size = New System.Drawing.Size(181, 22)
+        Me.KeepFrontMenuItem.Text = "Keep Front"
+        '
+        'HideToolToolStripMenuItem
+        '
+        Me.HideToolToolStripMenuItem.Name = "HideToolToolStripMenuItem"
+        Me.HideToolToolStripMenuItem.Size = New System.Drawing.Size(181, 22)
+        Me.HideToolToolStripMenuItem.Text = "Hide in Taskbar"
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(181, 22)
+        Me.ToolStripMenuItem1.Text = "Font"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(181, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'EvText
+        '
+        Me.EvText.Font = New System.Drawing.Font("微软雅黑", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.EvText.Location = New System.Drawing.Point(5, 61)
+        Me.EvText.Multiline = True
+        Me.EvText.Name = "EvText"
+        Me.EvText.Size = New System.Drawing.Size(385, 290)
+        Me.EvText.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CausesValidation = False
+        Me.ClientSize = New System.Drawing.Size(679, 363)
+        Me.Controls.Add(Me.EvText)
+        Me.Controls.Add(Me.CmbSelect)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Easy Note"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents CmbSelect As System.Windows.Forms.ComboBox
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents OperateToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ClearThisToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ClearAllToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents HideToolToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem1 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents FontDlg As System.Windows.Forms.FontDialog
+    Friend WithEvents EnglishToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ChineseToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents KeepFrontMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents EvText As System.Windows.Forms.TextBox
+
+End Class
---
+++ EasyNote/Form1.vb
@@ -0,0 +1,221 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Private CN_String(0 To 3) As String
+    Private EN_String(0 To 3) As String
+    Private HumanOperateChange As Boolean
+    Private LanguageIsEn As Boolean
+    Private IWR As New IniWR
+
+    Private Const WidthRem = 25
+    Private Const HeightRem = 50
+    Private FilePath As String
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim i As Int16
+
+        HumanOperateChange = False
+
+        FilePath = Application.StartupPath & "\Personal.ini"
+
+        CN_String(0) = "紧急且重要"
+        CN_String(1) = "紧急不重要"
+        CN_String(2) = "重要不紧急"
+        CN_String(3) = "既不紧急也不重要"
+
+        EN_String(0) = "Important And Emergency"
+        EN_String(1) = "Not Important But Emergency"
+        EN_String(2) = "Important And Not Emergency"
+        EN_String(3) = "Neither Important Nor Emergency"
+
+        CmbSelect.Items.Clear()
+        For i = 0 To 3
+            CmbSelect.Items.Add(EN_String(i))
+        Next i
+        CmbSelect.SelectedIndex = 0
+        'LanguageIsEn = True
+        Me.MinimizeBox = False
+
+        ReadSettings()
+    End Sub
+
+    Private Sub CmbSelect_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CmbSelect.SelectedIndexChanged
+        HumanOperateChange = True
+        Dim ReadStr As String
+        ReadStr = IWR.GetINI("Text", "LOG" & CStr(CmbSelect.SelectedIndex), "", FilePath)
+        EvText.Text = Replace(ReadStr, Chr(23), vbCrLf)
+        HumanOperateChange = False
+    End Sub
+
+    Private Sub ToolStripMenuItem1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripMenuItem1.Click
+        Dim res As System.Windows.Forms.DialogResult
+        res = FontDlg.ShowDialog()
+        If Not (res = System.Windows.Forms.DialogResult.Cancel) Then
+            EvText.Font = FontDlg.Font
+        End If
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ExitToolStripMenuItem.Click
+        UpdateSettings()
+        End
+    End Sub
+
+    Private Sub HideToolToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles HideToolToolStripMenuItem.Click
+        Me.ShowInTaskbar = Not Me.ShowInTaskbar
+        Execute_Rules()
+        UpdateSettings()
+    End Sub
+
+    Private Sub ClearThisToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ClearThisToolStripMenuItem.Click
+        If MsgBox("Are you sure to clear this textbox?", vbYesNo) = MsgBoxResult.Yes Then
+            EvText.Text = ""
+        End If
+    End Sub
+
+    Private Sub EnglishToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles EnglishToolStripMenuItem.Click
+        Language_as_English()
+        UpdateSettings()
+    End Sub
+
+    Private Sub ChineseToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ChineseToolStripMenuItem.Click
+        Language_as_Chinese()
+        UpdateSettings()
+    End Sub
+
+    Private Sub Form1_Resize(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Resize
+
+        CmbSelect.Width = Me.Width - WidthRem
+        EvText.Width = Me.Width - WidthRem
+        EvText.Height = Me.Height - (CmbSelect.Top + CmbSelect.Height + HeightRem)
+    End Sub
+
+    Private Sub KeepFrontMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles KeepFrontMenuItem.Click
+        Me.TopMost = Not Me.TopMost
+        Call Execute_Rules()
+        UpdateSettings()
+    End Sub
+
+    Private Sub Language_as_Chinese()
+        Dim NS As Int16
+        LanguageIsEn = False
+        NS = CmbSelect.SelectedIndex
+        CmbSelect.Items.Clear()
+        For i = 0 To 3
+            CmbSelect.Items.Add(CN_String(i))
+        Next i
+        CmbSelect.SelectedIndex = NS
+        OperateToolStripMenuItem.Text = "操作"
+        ClearThisToolStripMenuItem.Text = "清除此文本框"
+        ClearAllToolStripMenuItem.Text = "语言选择"
+        ToolStripMenuItem1.Text = "字体"
+        ExitToolStripMenuItem.Text = "退出"
+        Execute_Rules()
+        Me.Text = "简单笔记"
+    End Sub
+
+    Private Sub Language_as_English()
+        Dim NS As Int16
+        LanguageIsEn = True
+        NS = CmbSelect.SelectedIndex
+        CmbSelect.Items.Clear()
+        For i = 0 To 3
+            CmbSelect.Items.Add(EN_String(i))
+        Next i
+        CmbSelect.SelectedIndex = NS
+        OperateToolStripMenuItem.Text = "Operation"
+        ClearThisToolStripMenuItem.Text = "Clear This"
+        ClearAllToolStripMenuItem.Text = "Language"
+        ToolStripMenuItem1.Text = "Font"
+        ExitToolStripMenuItem.Text = "Exit"
+        Execute_Rules()
+        Me.Text = "Easy Note"
+    End Sub
+
+    Private Sub Execute_Rules()
+
+        If Me.ShowInTaskbar = True Then
+            If LanguageIsEn Then
+                HideToolToolStripMenuItem.Text = "Hide in Taskbar"
+            Else
+                HideToolToolStripMenuItem.Text = "在任务栏中隐藏"
+            End If
+
+        Else
+            If LanguageIsEn Then
+                HideToolToolStripMenuItem.Text = "Show in Taskbar"
+            Else
+                HideToolToolStripMenuItem.Text = "在任务栏中显示"
+            End If
+        End If
+
+        If Me.TopMost = True Then
+            If LanguageIsEn Then
+                KeepFrontMenuItem.Text = "Discard Keep Front"
+            Else
+                KeepFrontMenuItem.Text = "取消最前"
+            End If
+
+        Else
+            If LanguageIsEn Then
+                KeepFrontMenuItem.Text = "Keep Front"
+            Else
+                KeepFrontMenuItem.Text = "始终最前"
+            End If
+        End If
+
+    End Sub
+
+    Private Sub ReadSettings()
+        Dim IWR As New IniWR
+
+        LanguageIsEn = Str2Bool(IWR.GetINI("Sets", "Language", "1", FilePath))
+
+        If LanguageIsEn Then
+            Language_as_English()
+        Else
+            Language_as_Chinese()
+        End If
+
+        Me.TopMost = Str2Bool(IWR.GetINI("Sets", "TopMost", "0", FilePath))
+
+        Me.ShowInTaskbar = Str2Bool(IWR.GetINI("Sets", "ShowInTaskbar", "1", FilePath))
+
+        Execute_Rules()
+
+    End Sub
+
+    Private Sub UpdateSettings()
+        '保存在前，隐藏和语言
+
+        IWR.WriteINI("Sets", "Language", Bool2String(LanguageIsEn), FilePath)
+        IWR.WriteINI("Sets", "TopMost", Bool2String(Me.TopMost), FilePath)
+        IWR.WriteINI("Sets", "ShowInTaskbar", Bool2String(Me.ShowInTaskbar), FilePath)
+    End Sub
+
+    Private Function Bool2String(ByVal InputVal As Boolean) As String
+        If InputVal Then
+            Bool2String = "1"
+        Else
+            Bool2String = "0"
+        End If
+    End Function
+
+    Private Function Str2Bool(ByVal InputVal As String) As Boolean
+        If InputVal = "0" Then
+            Str2Bool = False
+        Else
+            Str2Bool = True
+        End If
+    End Function
+
+
+    Private Sub EvText_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles EvText.TextChanged
+        If Not HumanOperateChange Then
+            Dim WriteStr As String
+            WriteStr = Replace(EvText.Text, vbCrLf, Chr(23))
+            IWR.WriteINI("Text", "LOG" & CStr(CmbSelect.SelectedIndex), WriteStr, FilePath)
+        End If
+    End Sub
+End Class
